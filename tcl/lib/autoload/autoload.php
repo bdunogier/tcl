a06332d55f7225e554334af1fa92596288b787b5
Bertrand Dunogier <bertrand.dunogier@gmail.com>
2010-05-28 18:25:46 +0200

    Implemented some TCL stuff in there. Crappy.

--- tcl/lib/autoload/autoload.php
+++ tcl/lib/autoload/autoload.php
@@ -1,8 +1,12 @@
 <?php
 return array(
+	// MVC related
 	'tclMvcConfiguration' => 'config.php',
 	'tclMvcRouter'        => 'router.php',
 	'tclController'       => 'controllers/main.php',
 	'tclHttpView'         => 'views/http.php',
+
+	// TCL related
+	'tclHoraires'         => 'tcl/horaires.php',
 );
 ?>
--- tcl/lib/controllers/main.php
+++ tcl/lib/controllers/main.php
@@ -5,7 +5,24 @@ class tclController extends ezcMvcController
 	{
 		$res = new ezcMvcResult;
 		$res->variables['test'] = 'test';
-		return $res;
+
+	    /* temporary junk */
+	    $tcl = new tclHoraires( new tclParamsHorairesL10 );
+        foreach( $tcl->horaires() as $match )
+        {
+            if ( str_replace( 'h' , '', $match[1] ) > $currentTime['t'] )
+            {
+                $horaire = $match[1];
+                $tempsRestant = ( ( $match[3] - $currentTime['h'] ) * 60 ) + ( $match[4] - $currentTime['m'] );
+
+                echo "10 à $horaire (dans $tempsRestant minutes)<br />";
+                $displayed++;
+            }
+            if ( $displayed == 3 )
+                break;
+        }
+
+        return $res;
 	}
 
 	public function doRoot()
