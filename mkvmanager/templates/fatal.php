5e5b3ebbc228160a628945be3dd4e60edb89b91e
Bertrand Dunogier <bd@ez.no>
2011-01-15 19:57:33 +0100

    Removed mkvmanager, moved to its own repository: https://github.com/bdunogier/mkvmanager

--- mkvmanager/templates/fatal.php
+++
@@ -1,2 +0,0 @@
-<h1>An exception has occured</h1>
-<pre><? print_r( $this->exception ) ?></pre>
--- mkvmanager/templates/movies-without-nfo.php
+++
@@ -1,4 +0,0 @@
-<h1>Movies without NFO</h1>
-<ul><? foreach( $this->movies as $movie ) :?>
-    <li><?=htmlspecialchars( $movie )?></li>
-<? endforeach?></ul>
